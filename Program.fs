7e2e8e28e825a5663e4a1c5dc90ef22bf089b974
Josh <zylo.codes@gmail.com>
2022-06-04 14:09:27 -0500

    rps - cont'd

--- Program.fs
+++ Program.fs
@@ -1,19 +1,77 @@
-﻿module PigLatin =
-  let private isVowel c =
-    match c with 
-    | 'a' | 'e' | 'i' | 'o' | 'u'
-    | 'A' | 'E' | 'I' | 'O' | 'U' -> true
-    |_ -> false
+﻿module RPS =
+  let choices = ["r"; "p"; "s"]
+  let mutable roundNum = 0
+  let mutable keepPlaying = true
+  let mutable wins = 0
+  let mutable losses = 0
+  let mutable ties = 0
 
-  let toPigLatin (word: string) =
-    if isVowel word[0] then
-      word + "yay"
+  let getComputerChoice () : string =
+    let rand = new System.Random()
+    choices.[rand.Next(choices.Length)]
+
+  let isValidChoice (str : string) : bool =
+    match str with
+    | "r" | "p" | "s" -> true
+    | _ -> false
+
+  let compareChoices (userChoice: string, computerChoice: string) : string =
+    if (System.String.Compare(userChoice, "r") > -1 && System.String.Compare(computerChoice, "s") > -1) || (System.String.Compare(userChoice, "p") > -1 && System.String.Compare(computerChoice, "r") > -1) || (System.String.Compare(userChoice, "s") > -1 && System.String.Compare(computerChoice, "p") > -1) then
+      "win"
+    elif System.String.Compare(userChoice, computerChoice) > -1 then 
+      "tie"
     else
-      word[1..] + string(word[0]) + "ay"
+      "lose"
+
+  let updateScore (result: string) =
+    if System.String.Compare(result, "win") > -1 then
+      wins <- wins + 1
+    elif System.String.Compare(result, "tie") > -1 then
+      ties <- ties + 1
+    else
+      losses <- losses + 1
+
+  let playAgainPrompt () : bool =
+    printf("Want to keep playing? [y/n]")
+    let answer = System.Console.ReadLine()
+    match answer with
+    | "n" -> false
+    | _ -> true
+
+  let checkGameOver () =
+    let mutable msg = ""
+    if wins > 2 then
+      msg <- "win"
+    elif ties > 2 then 
+      msg <- "tie"
+    elif losses > 2 then
+      msg <- "lose"
+    let gameOver = 
+      match msg with
+        | "" -> false
+        | _ -> true
+    if gameOver then
+      printf "GAME OVER. You %s!\n" msg
+      keepPlaying <- playAgainPrompt()
+
+  let rpsRound () =
+    printf "===================================\n"
+    printf "ROUND %i" 
+    printf("Choose 'r', 'p', or 's']: ")
+    let userChoice = System.Console.ReadLine()
+    let computerChoice = getComputerChoice()
+    if isValidChoice(userChoice) then 
+      let result = compareChoices(userChoice, computerChoice)
+      printf "You chose: %s\nComputer chose: %s\n" userChoice computerChoice
+      printf "-----------------------------------\n"
+      updateScore(result)
+      printf "Score:\nWins: %i Tie: %i Losses: %i\n" wins ties losses
+      printf "===================================\n"
+      checkGameOver()
+      roundNum <- roundNum + 1
 
 [<EntryPoint>]
 let main args = 
-  for arg in args do
-    let newArg = PigLatin.toPigLatin arg
-    printfn "%s in Pig latin is %s" arg newArg
+  while RPS.keepPlaying do
+    RPS.rpsRound()
   0
--- RPSSandbox.fsx
+++ RPSSandbox.fsx
@@ -1,41 +1,82 @@
-// let choices: (string * string * string)[] = [|"r", "p", "s"|]
 let choices = ["r"; "p"; "s"]
+let mutable roundNum = 1
 let mutable keepPlaying = true
 let mutable wins = 0
 let mutable losses = 0
 let mutable ties = 0
 
-let getRandomChoice () : string =
+let getComputerChoice () : string =
   let rand = new System.Random()
   choices.[rand.Next(choices.Length)]
 
-let validateChoice (str : string) : bool =
+let isValidChoice (str : string) : bool =
   match str with
   | "r" | "p" | "s" -> true
   | _ -> false
 
+let getUserChoice () : string =
+  printf("Choose r, p, s (q to quit): ")
+  let choice = System.Console.ReadLine()
+
+  if System.String.Compare(choice, "exit") = 0 then
+    System.Environment.Exit(0)
+  choice
+
 let compareChoices (userChoice: string, computerChoice: string) : string =
-  if System.String.Compare(userChoice, "r") > -1 && System.String.Compare(computerChoice, "s") > -1 || System.String.Compare(userChoice, "p") > -1 && System.String.Compare(computerChoice, "r") > -1 || System.String.Compare(userChoice, "s") > -1 && System.String.Compare(computerChoice, "p") > -1 then
+  if (System.String.Compare(userChoice, "r") = 0 && System.String.Compare(computerChoice, "s") = 0) || (System.String.Compare(userChoice, "p") = 0 && System.String.Compare(computerChoice, "r") = 0) || (System.String.Compare(userChoice, "s") = 0 && System.String.Compare(computerChoice, "p") = 0) then
     "win"
-  elif System.String.Compare(userChoice, computerChoice) > -1 then 
+  elif System.String.Compare(userChoice, computerChoice) = 0 then 
     "tie"
   else
     "lose"
 
+let updateScore (result: string) =
+  if System.String.Compare(result, "win") > -1 then
+    wins <- wins + 1
+  elif System.String.Compare(result, "tie") > -1 then
+    ties <- ties + 1
+  else
+    losses <- losses + 1
+
+let playAgainPrompt () : bool =
+  printf("Want to play again? [y/n]\n")
+  let answer = System.Console.ReadLine()
+  match answer with
+  | "n" -> false
+  | _ -> true
+
+let checkGameOver () =
+  let mutable msg = ""
+  if wins > 2 then
+    msg <- "win"
+  elif ties > 2 then 
+    msg <- "tie"
+  elif losses > 2 then
+    msg <- "lose"
+  let gameOver = 
+    match msg with
+      | "" -> false
+      | _ -> true
+  if gameOver then
+    printf "GAME OVER. You %s!\n" msg
+    keepPlaying <- playAgainPrompt()
+
+let rpsRound () =
+  printf "ROUND %i\n" roundNum 
+  let userChoice = getUserChoice()
+  let computerChoice = getComputerChoice()
+  if isValidChoice(userChoice) then 
+    let result = compareChoices(userChoice, computerChoice)
+    printf "You: %s Computer: %s\n" userChoice computerChoice
+    printf "-----------------------------------\n"
+    updateScore(result)
+    printf "Score:\nWins: %i Tie: %i Losses: %i\n" wins ties losses
+    printf "===================================\n"
+    checkGameOver()
+    roundNum <- roundNum + 1
+  else
+    printf "Bad input, try again.\n"
+
 // game loop
 while keepPlaying do
-  printf("Choose 'r', 'p', or 's']: ")
-  let userChoice = System.Console.ReadLine()
-  let computerChoice = getRandomChoice()
-  if validateChoice(userChoice) then 
-    let result = compareChoices(userChoice, computerChoice)
-    if System.String.Compare(result, "win") > -1 then
-      wins <- wins + 1
-    elif System.String.Compare(result, "tie") > -1 then
-      ties <- ties + 1
-    else
-      losses <- losses + 1
-    printfn "Score:\nWins: %i\nTie: %i\nLosses: %i" wins ties losses
-    // keepPlaying <- true
-  else
-    keepPlaying <- false
+  rpsRound()
